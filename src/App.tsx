3890afc4648c45ea76dc818abeb8073748221d3f
bugxd <l.feistritzer@gmx.at>
2020-07-27 08:22:10 +0200

    init
    initial commit: pdf

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { PDFViewer, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+function App() {
+  return (
+    <div className="App">
+      <PDFViewer
+        style={{
+          width:"100%",
+          height:"100%"
+        }}
+      >
+        <MyDocument/>
+      </PDFViewer>
+    </div>
+  );
+}
+
+export default App;
